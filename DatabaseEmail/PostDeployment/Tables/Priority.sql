6b5e16405e105982df33063953f056fb61103faf
showmen15@poczta.onet.pl <showmen15@poczta.onet.pl>
2019-10-23 07:37:35 +0200

    logika bazodanowa

---
+++ DatabaseEmail/PostDeployment/Tables/Priority.sql
@@ -0,0 +1,36 @@
+﻿SET NOCOUNT ON
+GO
+
+MERGE INTO [dbo].[Priority] AS [Target]
+USING (VALUES
+  (0,N'Normal')
+ ,(1,N'Low')
+ ,(2,N'High')
+) AS [Source] ([IdPriority],[Desc])
+ON ([Target].[IdPriority] = [Source].[IdPriority])
+WHEN MATCHED AND (
+	NULLIF([Source].[Desc], [Target].[Desc]) IS NOT NULL OR NULLIF([Target].[Desc], [Source].[Desc]) IS NOT NULL) THEN
+ UPDATE SET
+  [Target].[Desc] = [Source].[Desc]
+WHEN NOT MATCHED BY TARGET THEN
+ INSERT([IdPriority],[Desc])
+ VALUES([Source].[IdPriority],[Source].[Desc])
+WHEN NOT MATCHED BY SOURCE THEN 
+ DELETE;
+
+DECLARE @mergeError int
+ , @mergeCount int
+SELECT @mergeError = @@ERROR, @mergeCount = @@ROWCOUNT
+IF @mergeError != 0
+ BEGIN
+ PRINT 'ERROR OCCURRED IN MERGE FOR [dbo].[Priority]. Rows affected: ' + CAST(@mergeCount AS VARCHAR(100)); -- SQL should always return zero rows affected
+ END
+ELSE
+ BEGIN
+ PRINT '[dbo].[Priority] rows affected by MERGE: ' + CAST(@mergeCount AS VARCHAR(100));
+ END
+GO
+
+
+SET NOCOUNT OFF
+GO
---
+++ DatabaseEmail/PostDeployment/Tables/Types.sql
@@ -0,0 +1,36 @@
+﻿SET NOCOUNT ON
+
+MERGE INTO [dbo].[Types] AS [Target]
+USING (VALUES
+  (0,N'Sender')
+ ,(1,N'To')
+ ,(2,N'Cc')
+ ,(3,N'Bcc')
+) AS [Source] ([IdType],[Desc])
+ON ([Target].[IdType] = [Source].[IdType])
+WHEN MATCHED AND (
+	NULLIF([Source].[Desc], [Target].[Desc]) IS NOT NULL OR NULLIF([Target].[Desc], [Source].[Desc]) IS NOT NULL) THEN
+ UPDATE SET
+  [Target].[Desc] = [Source].[Desc]
+WHEN NOT MATCHED BY TARGET THEN
+ INSERT([IdType],[Desc])
+ VALUES([Source].[IdType],[Source].[Desc])
+WHEN NOT MATCHED BY SOURCE THEN 
+ DELETE;
+
+DECLARE @mergeError int
+ , @mergeCount int
+SELECT @mergeError = @@ERROR, @mergeCount = @@ROWCOUNT
+IF @mergeError != 0
+ BEGIN
+ PRINT 'ERROR OCCURRED IN MERGE FOR [dbo].[Types]. Rows affected: ' + CAST(@mergeCount AS VARCHAR(100)); -- SQL should always return zero rows affected
+ END
+ELSE
+ BEGIN
+ PRINT '[dbo].[Types] rows affected by MERGE: ' + CAST(@mergeCount AS VARCHAR(100));
+ END
+GO
+
+
+SET NOCOUNT OFF
+GO
